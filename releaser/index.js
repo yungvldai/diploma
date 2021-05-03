ec941eb6b597addf0262dc0eca9fa1696878129a
Vlad Ivanov <vladislav.ivanov@onetwotrip.com>
2021-05-03 15:38:20 +0300

    Релиз от 03 май 2021 в 15:38

---
+++ releaser/index.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const chalk = require('chalk');
+const fs = require('fs');
+const merge = require('easy-pdf-merge');
+
+const getReleaseDate = () => {
+  return Date.now();
+}
+
+const paths = {
+  mainFile: path.resolve(__dirname, '../document/production.pdf'),
+  pdfsFolder: path.resolve(__dirname, '../pdfs/'),
+  config: path.resolve(__dirname, '../pdfs/config.json'),
+  releaseOut: path.resolve(__dirname, `../releases/${getReleaseDate()}.pdf`)
+};
+
+const getConfig = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(paths.config, 'utf8', (error, data) => {
+      if (error) {
+        reject(error);
+        return
+      }
+      resolve(data);
+    })
+  })
+}
+
+const parseConfig = (config) => {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(JSON.parse(config));
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+const mergePdfs = (files, out) => {
+  return new Promise((resolve, reject) => {
+    merge(files, out, (error) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      
+      resolve(null);
+    });
+  })
+}
+
+(async () => {
+  try {
+    const config = await getConfig();
+    const parsed = await parseConfig(config);
+
+    const files = parsed.order.map(name => {
+      if (name === 'MAIN') {
+        return paths.mainFile;
+      }
+
+      return path.resolve(paths.pdfsFolder, name);
+    });
+
+    await mergePdfs(files, paths.releaseOut);
+    console.log(chalk.green('OK!'), paths.releaseOut);
+  } catch (error) {
+    console.log(chalk.red('Something went wrong!'));
+    console.log(error);
+  }
+})();
---
+++ scripts/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#get highest tag number
+VERSION=`git describe --abbrev=0 --tags`
+
+#replace . with space so can split into an array
+VERSION_BITS=(${VERSION//./ })
+
+#get number parts and increase last one by 1
+VNUM1=${VERSION_BITS[0]}
+VNUM2=${VERSION_BITS[1]}
+VNUM3=${VERSION_BITS[2]}
+VNUM3=$((VNUM3+1))
+
+#create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+
+echo "New version: $NEW_TAG"
+
+git add . && git commit -m "Релиз от $(date +'%d %b %Y в %H:%M')" && git tag $NEW_TAG
